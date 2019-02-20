da559201a0d40b8ea39e074337634d9571cc7da9
Taimoor Hassan <tempetationx@hotmail.com>
2019-02-20 15:26:17 +0500

    Added portfolio item via esource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_100857) do
+ActiveRecord::Schema.define(version: 2019_02_20_102219) do
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "title"
