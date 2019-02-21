26b46e930ce72e3de75019c19c35a907a8cc7a4e
Taimoor Hassan <tempetationx@hotmail.com>
2019-02-21 11:41:04 +0500

    configure root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  root to: 'pages#home'
+  get 'about-me', to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
