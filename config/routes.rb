c1919bf3a523499a1f69086676c55cdfd9753b84
Taimoor Hassan <tempetationx@hotmail.com>
2019-02-21 13:51:15 +0500

    added status

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  
+
   root to: 'pages#home'
   get 'about-me', to: 'pages#home'
   get 'contact', to: 'pages#contact'
