7d1ed518a54a722e851fecdb5db8895dd46c5ad9
Taimoor Hassan <tempetationx@hotmail.com>
2019-02-21 12:19:47 +0500

    friendly reoutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :blogs
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  
   root to: 'pages#home'
   get 'about-me', to: 'pages#home'
   get 'contact', to: 'pages#contact'
