fb590b567ed8ff648b0cdedb4670642f184aafe0
Taimoor Hassan <tempetationx@hotmail.com>
2019-02-21 12:50:57 +0500

    updated status for enum

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,19 @@ puts "10 skill post created"
 end
 
 puts "9 portfolio created"
+
+
+10.times do |blog|
+    Blog.create!(
+        title: "Blog Title: #{blog}",
+        body: "unde omnis iste natus error sit voluptatem accusantium 
+        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo 
+        inventore veritatis et quasi architecto beatae vitae dicta sunt 
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
+        aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
+        ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui 
+        dolorem ipsum quia dolor sit amet, co"
+    )
+end
+
+puts "10 Blogs created"
