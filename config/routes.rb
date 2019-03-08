56d2f63b21659385650e51b2fd54e19a8861b132
Taimoor Hassan <tempetationx@hotmail.com>
2019-03-08 15:02:45 +0500

    styles home edit login

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   root to: 'pages#home'
-  get 'about-me', to: 'pages#home'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
